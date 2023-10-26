f7fdd18f069e497415b72c299a7450fa3c874759
Asadullah Chowdhury Rahat <asadullah_chowdhury@outlook.com>
2023-10-26 14:08:01 +0600

    [bm-9]-vite js file update

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [
         vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/front/app.js', 'resources/scss/front/style.scss'],
             refresh: true,
         }),
     ],
